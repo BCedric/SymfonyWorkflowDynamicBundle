393a3a6433d6123ce49c600190d6121749729926
Cedbois <cedric.bois@uca.fr>
2026-05-06 09:43:49 +0200

    load services config

--- src/BCedricSymfonyWorkflowDynamicBundle.php
+++ src/BCedricSymfonyWorkflowDynamicBundle.php
@@ -12,5 +12,8 @@ class BCedricSymfonyWorkflowDynamicBundle extends AbstractBundle
 
     public function configure(DefinitionConfigurator $definition): void {}
 
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void {}
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
 }
